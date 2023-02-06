864b605f0a04658c61fe7cf0b4fc264c5a7e7153
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 23:24:44 +0300

    vue_007

--- vue3abc/src/App.vue
+++ vue3abc/src/App.vue
@@ -1,32 +1,30 @@
 <script>
-  export default{
-    name:'App',
-	data() {
-		return {
-      num: 5,
-      num: 1,
-      num: 2,
-      num: 3,
-      var1: 1,
-      var2: 2,
-      Text: 'page',
-      href: 'page.html',
-      attr: 'hi.png',
-		name: 'Edem',
+  export default {
+    data() {
+	return {
+		name: 'Edemkaaa',
+    group: 'I-1-21',
     surname: 'Asanov',
-		group: 'I-1-21',
-		}
+    attr: 'hi.png',
+    text: 'page',
+		href: 'page.html',
+    num: 5,
+    num1: 1,
+		num2: 2,
+		num3: 3,
+    arr1: ['x', 'y', 'z'], 
+    arr2: [1, 2, 3], 
 	}
 }
+  }
 </script>
 
 <template>
-  {{ var1 + var2 }}
 <div> 
   <a href="page.html">page</a>
+  <p class="sc">Сумма {{num1+num2+num3}}.</p>
     <p> Привет, меня зовут {{name}} я из группы {{group}} а моя фамилия {{ surname }} !</p>
  <img v-bind:src="attr">
- <p>{{ num1 + num2 + num3 }}</p>
  </div>
 </template>
 
