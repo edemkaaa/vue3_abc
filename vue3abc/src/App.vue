609119198339b9a9d3648d163e18fe0376ec9926
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 23:12:29 +0300

    vue_006

--- vue3abc/src/App.vue
+++ vue3abc/src/App.vue
@@ -3,6 +3,12 @@
     name:'App',
 	data() {
 		return {
+      num: 5,
+      num: 1,
+      num: 2,
+      num: 3,
+      var1: 1,
+      var2: 2,
       Text: 'page',
       href: 'page.html',
       attr: 'hi.png',
@@ -15,10 +21,12 @@
 </script>
 
 <template>
+  {{ var1 + var2 }}
 <div> 
   <a href="page.html">page</a>
     <p> Привет, меня зовут {{name}} я из группы {{group}} а моя фамилия {{ surname }} !</p>
  <img v-bind:src="attr">
+ <p>{{ num1 + num2 + num3 }}</p>
  </div>
 </template>
 
