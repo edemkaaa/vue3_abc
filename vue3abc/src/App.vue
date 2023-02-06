2db2b6258fce0b8c6348e23784845571b7b61701
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 23:32:11 +0300

    vue_009

--- vue3abc/src/App.vue
+++ vue3abc/src/App.vue
@@ -13,17 +13,26 @@
 		num2: 2,
 		num3: 3,
     arr1: ['x', 'y', 'z'], 
-    arr2: [1, 2, 3], 
+    arr2: [1, 2, 3],
+    obj: {x: 5, y: 10, z: 15},
 	}
+},
+methods: {
+show: function() {
+alert(today);
 }
-  }
+}
+}
+let today = new Date();
 </script>
 
 <template>
+  {{ show() }}
 <div> 
   <a href="page.html">page</a>
-  <p class="sc">Сумма {{num1+num2+num3}}.</p>
     <p> Привет, меня зовут {{name}} я из группы {{group}} а моя фамилия {{ surname }} !</p>
+    <p class="sc">Сумма {{num1+num2+num3}}.</p>
+    <p class="sc">Сумма {{obj.x + obj.y +obj.z}}.</p>
  <img v-bind:src="attr">
  </div>
 </template>
