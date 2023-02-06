6a3e28a2eacc8007f9d4479094aeaaf892cd9f1d
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 23:05:54 +0300

    vue_005

--- vue3abc/src/App.vue
+++ vue3abc/src/App.vue
@@ -3,6 +3,9 @@
     name:'App',
 	data() {
 		return {
+      Text: 'page',
+      href: 'page.html',
+      attr: 'hi.png',
 		name: 'Edem',
     surname: 'Asanov',
 		group: 'I-1-21',
@@ -13,6 +16,16 @@
 
 <template>
 <div> 
-    <h1> Привет, меня зовут {{name}} я из группы {{group}} а моя фамилия {{ surname }} !</h1>
-  </div>
+  <a href="page.html">page</a>
+    <p> Привет, меня зовут {{name}} я из группы {{group}} а моя фамилия {{ surname }} !</p>
+ <img v-bind:src="attr">
+ </div>
 </template>
+
+<style>
+p {
+  font-size: large;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: rgb(104, 62, 62);
+  }
+  </style>
