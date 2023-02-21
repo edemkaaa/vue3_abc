6b86f055c16041fe6d8b354a25a753e5c4937889
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 20:10:55 +0300

    vue_051

--- vue3abc/src/App.vue
+++ vue3abc/src/App.vue
@@ -2,42 +2,26 @@
   export default{
     data() {
       return {
-		selected: 'value1',
-		options: ['Понедельник', 'Вторник', 'Среда','Четверг','Пятница','Суббота','Воскресенье'],
-    days: '1',
-    options1: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',' 31'],
-    month: 'Январь',
-    options2: ['Январь', 'Февраль','Март', 'Апрель', 'Май','Июнь', 'Июль', 'Август', 'Сентябрь','Октябрь', 'Ноябрь', 'Декабрь'],
-    years: '2023',
-    options3: ['2001', '2002','2003', '2004', '2005','2006']
+        isDisabled: true,
+        isDisabled1: true,
 	}
 },
 methods: {
-
+  toggle: function () {
+    this.isDisabled = false;
+  },
+  toggle1: function () {
+    this.isDisabled1 = false;
+  },
 }
 }
 </script>
 
 <template>
 <ul>
-	<select v-model="selected">
-		<option v-for="option in options">{{ option }}</option>
-	</select>
-	<p>{{ selected }}</p>
-  <br>
-  <p class="button">Выберите день своего рождения</p>
-  <br>
-  <select v-model="days">
-  <option v-for="option1 in options1">{{ option1 }}</option>
-</select>
-<br>
-<select v-model="month">
-  <option v-for="option2 in options2">{{ option2 }}</option>
-</select>
-<br>
-<select v-model="years">
-  <option v-for="option3 in options3">{{ option3 }}</option>
-</select>
+	<button v-bind:disabled="isDisabled">btn</button>
+	<button v-bind:disabled="isDisabled">btn</button>
+  <button class="button" @click="toggle">btn</button>
 </ul>
 </template>
 
